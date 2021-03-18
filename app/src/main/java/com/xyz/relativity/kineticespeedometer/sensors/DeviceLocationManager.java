af3fe551f5d2d4697cb9bf14f1629864b8b3c5d3
xyz <xyz@arclimbing.org>
2021-03-18 07:39:37 -0400

    Remove dependency on google play code.

---
+++ app/src/main/java/com/xyz/relativity/kineticespeedometer/sensors/DeviceLocationManager.java
@@ -0,0 +1,49 @@
+package com.xyz.relativity.kineticespeedometer.sensors;
+
+import android.location.Location;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xyz on 12/6/17.
+ */
+
+public class DeviceLocationManager implements FuseLocationProvider.LocationEvent {
+	private AppCompatActivity parent;
+	private final List<ILocationListener> eventsHandler = new ArrayList<>();
+	private final FuseLocationProvider fusedLocationManager;
+
+	public DeviceLocationManager(AppCompatActivity parent, int intervalMs, ILocationListener pEventsHandler) {
+		this.parent = parent;
+		addListener(pEventsHandler);
+
+		fusedLocationManager = new FuseLocationProvider(parent, intervalMs, this);
+	}
+
+	public void addListener(ILocationListener... pEventsHandler) {
+		for (ILocationListener i : pEventsHandler) {
+			if (!eventsHandler.contains(i)) {
+				eventsHandler.add(i);
+			}
+		}
+
+	}
+
+	public void onResume() {
+		fusedLocationManager.onResume();
+	}
+
+	public void onPause() {
+		fusedLocationManager.onPause();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		for (ILocationListener client : eventsHandler) {
+			client.updatePosition(location);
+		}
+	}
+}
---
+++ app/src/main/java/com/xyz/relativity/kineticespeedometer/sensors/FuseLocationProvider.java
@@ -0,0 +1,102 @@
+package com.xyz.relativity.kineticespeedometer.sensors;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.concurrent.TimeUnit;
+
+public class FuseLocationProvider implements LocationListener {
+	private static final float MINIMUM_DISTANCE_METERS = 1f;
+
+	private final LocationEvent eventListener;
+	private final int intervalMs;
+
+	public interface LocationEvent {
+		void onLocationChanged(Location location);
+	}
+
+	private final LocationManager locationManager;
+	private final AppCompatActivity parent;
+	private Location lastLocation;
+
+	public FuseLocationProvider(AppCompatActivity parent, int intervalMs, LocationEvent eventListener) {
+		this.parent = parent;
+		this.eventListener = eventListener;
+		this.intervalMs = intervalMs;
+
+		this.locationManager = (LocationManager) parent
+				.getSystemService(Context.LOCATION_SERVICE);
+
+		if (ActivityCompat.checkSelfPermission(parent, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+				&& ActivityCompat.checkSelfPermission(parent, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			return;
+		}
+		lastLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+		if (lastLocation != null) {
+			updateListeners();
+		}
+	}
+
+	public void onResume() {
+		if (ActivityCompat.checkSelfPermission(parent, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+				&& ActivityCompat.checkSelfPermission(parent, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+			return;
+		}
+
+		for (String providerStr: locationManager.getAllProviders()) {
+			if (providerStr.equalsIgnoreCase(LocationManager.PASSIVE_PROVIDER)) {
+				continue;
+			}
+
+			locationManager.requestLocationUpdates(providerStr, intervalMs,
+					MINIMUM_DISTANCE_METERS, FuseLocationProvider.this);
+		}
+	}
+
+	public void onPause() {
+		locationManager.removeUpdates(this);
+	}
+
+	private void updateListeners() {
+		eventListener.onLocationChanged(lastLocation);
+	}
+
+	@Override
+	public void onLocationChanged(@NonNull Location location) {
+		System.out.println("NEW LOCATION " + location);
+		long compareTime = SystemClock.elapsedRealtimeNanos();
+		float oldLocationAccuracy = Math.max(0, lastLocation.getAccuracy() + TimeUnit.NANOSECONDS.toSeconds(compareTime - lastLocation.getElapsedRealtimeNanos()));
+		float newLocationAccuracy = Math.max(0, location.getAccuracy() + TimeUnit.NANOSECONDS.toSeconds(compareTime - location.getElapsedRealtimeNanos()));
+
+		System.out.println("oldAcc: " + oldLocationAccuracy + " newAcc: " + newLocationAccuracy + " swap: " + (newLocationAccuracy <= oldLocationAccuracy));
+		if (newLocationAccuracy <= oldLocationAccuracy) {
+			lastLocation = new Location(location);
+		}
+		updateListeners();
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+
+	}
+
+	@Override
+	public void onProviderEnabled(@NonNull String provider) {
+
+	}
+
+	@Override
+	public void onProviderDisabled(@NonNull String provider) {
+
+	}
+}
